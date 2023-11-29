f1e295fc26a1698d77c866192c78eafb87e8b1ba
Willzhang13 <yizhang@gatech.edu>
2023-11-28 23:49:58 -0500

    Teleportation

---
+++ Assets/Scripts/Teleportation.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+    public Transform player;
+    public Transform reciever;
+    private bool playerIsOverlapping = false;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+            if (dotProduct < 0f)           //Teleport
+            {
+                float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
+                rotationDiff += 180;
+                player.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                player.position = reciever.position + positionOffset;
+                
+            }
+            playerIsOverlapping = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+        }
+    }
+}
